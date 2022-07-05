a941df31f8c9e9a73e54f733c86bf812aa2aad7a
Barashnya <polinadorofeeva418@gmail.com>
2022-07-05 14:10:17 +0300

    Exercise27

---
+++ Exercise27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+Console.WriteLine("Print number: ");
+int number = int.Parse(Console.ReadLine());
+int count = 0;
+while(number != 0)
+{
+    count = count + (number%10);
+    number = number/10;
+}
+Console.WriteLine($"Result: {count}");
